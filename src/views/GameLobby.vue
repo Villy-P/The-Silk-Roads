b5d4514f3e6afce3236e1d9775f013fe2b09069c
Villy-P <valeriuspetrini@gmail.com>
2023-09-04 00:15:40 -0500

    Non Leader Lobby Screen

--- src/views/GameLobby.vue
+++ src/views/GameLobby.vue
@@ -2,16 +2,31 @@
     <div v-if="store.state.isLeader">
         <div class="w-screen h-32 flex items-center text-4xl font-mono py-10">
             <div class="grow px-5 text-ellipsis overflow-hidden whitespace-nowrap">{{ store.state.currentGame?.name }}</div>
-            <div class="px-5">{{ store.state.currentGame?.code }}</div>
+            <div class="px-5 flex flex-col justify-center">
+                <div class="text-sm text-center">Join Code:</div>
+                <div class="px-5">{{ store.state.currentGame?.code }}</div>
+            </div>
         </div><hr><br>
-        <div class="flex flex-wrap gap-2">
-            <div v-for="p in store.state.currentPlayers" :key="p ? p.id : ''" class="border-2 border-black p-1">
-                {{ p ? p.name : '' }}
+        <div class="flex flex-col gap-2 justify-center">
+            <div 
+                v-for="(p, i) in store.state.currentPlayers" 
+                :key="p ? p.id : ''" 
+                class="border-2 border-black p-1 py-2 m-4 w-96 max-w-96 text-center mx-auto"
+                :class="i == 0 ? 'bg-cyan-400 cursor-pointer hover:bg-cyan-500' : ''">
+                {{ p ? (i == 0 ? 'Start Game' : p.name) : '' }}
             </div>
         </div>
     </div>
     <div v-else>
-        {{store.state.currentGame?.name}}
+        <div class="text-center text-5xl py-5 px-3">
+            Welcome to {{ store.state.currentGame?.name }}
+        </div>
+        <div class="text-center text-lg px-3">
+            Edit your name and see if it shows up on your teachers screen.
+        </div>
+        <div class="my-5 text-center">
+            <input placeholder="Name" value="New Player" class="mx-auto pl-2 border-2 border-black py-1" minlength="1" maxlength="50">
+        </div>
     </div>
 </template>
 
