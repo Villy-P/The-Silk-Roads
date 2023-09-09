aa4b8253953e34330659eb4cc997d57ab0f302f8
Villy-P <valeriuspetrini@gmail.com>
2023-09-09 00:50:30 -0500

    Client Socket

---
+++ src/client/socket.ts
@@ -0,0 +1,9 @@
+import { IP_ADDRESS, SERVER_PORT } from '@/data/data';
+import io from 'socket.io-client'
+
+export const clientSocket = io(`https://${IP_ADDRESS}:${SERVER_PORT}/`, {
+    withCredentials: true,
+    extraHeaders: {
+        'custom-header': "abcd"
+    }
+});
