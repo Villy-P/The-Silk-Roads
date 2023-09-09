50f07cbde9291718a16f524a0f8924ab94a986d7
Villy-P <valeriuspetrini@gmail.com>
2023-09-09 00:47:30 -0500

    Listen on server

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import fs from 'fs';
 import https from 'https';
 import cors from 'cors';
+import { SERVER_PORT } from '@/data/data';
 
 const options = {
     key: fs.readFileSync('cert\\CA\\localhost\\localhost.decrypted.key', 'utf-8'),
@@ -15,4 +16,8 @@ app.use(cors({ origin: '*' }));
 
 app.get('/', (req, res) => {
     res.send("Successfully verified server!");
+});
+
+server.listen(SERVER_PORT, () => {
+    console.log("Express server listening on port " + SERVER_PORT);
 });
