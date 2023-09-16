74362b60e048f5765b53fa3c62632e0b5a86e144
Villy-P <valeriuspetrini@gmail.com>
2023-09-16 17:07:25 -0500

    Main Page tooltips

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,17 +1,25 @@
 <template>
     <div class="h-14 flex border-b-black border-b-2">
-        <div class="flex items-center justify-center w-fit">
+        <div class="flex items-center justify-center w-fit tooltip-container">
             <img src="../assets/items/silver.jpg" class="w-10">
-            <div>{{ store.state.user?.silver }}</div>
+            <div class="select-none">{{ store.state.user?.silver }}</div>
+            <div class="tooltip-text tooltip-bottom">Silver</div>
         </div>
-        <div class="flex items-center justify-center w-fit">
+        <div class="flex items-center justify-center w-fit tooltip-container">
             <img src="../assets/items/gold.jpg" class="w-10">
-            <div>{{ store.state.user?.gold }}</div>
+            <div class="select-none">{{ store.state.user?.gold }}</div>
+            <div class="tooltip-text tooltip-bottom">Gold</div>
         </div>
         <div class="flex items-center justify-center w-fit pl-5">
-            <div>{{ store.state.user?.items.length }}</div>
+            <div class="select-none tooltip-container h-full flex items-center">
+                <p>{{ store.state.user?.items.length }}</p>
+                <div class="tooltip-text tooltip-bottom">Items</div>
+            </div>
             <img src="../assets/economic.png" class="w-10">
-            <div>{{ store.state.user?.imports.length }}</div>
+            <div class="select-none tooltip-container h-full flex items-center">
+                <p>{{ store.state.user?.imports.length }}</p>
+                <div class="tooltip-text tooltip-bottom">Imports</div>
+            </div>
         </div>
     </div>
 </template>
@@ -26,3 +34,5 @@
         store = useStore(key);
     }
 </script>
+
+<style lang="scss" src="../styles/tooltip.scss"></style>
