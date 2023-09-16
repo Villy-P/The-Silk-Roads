29029a39d0dc18181032f473d8a6dd0c15118521
Villy-P <valeriuspetrini@gmail.com>
2023-09-16 17:15:27 -0500

    Show Image

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,25 +1,31 @@
 <template>
-    <div class="h-14 flex border-b-black border-b-2">
-        <div class="flex items-center justify-center w-fit tooltip-container">
-            <img src="../assets/items/silver.jpg" class="w-10">
-            <div class="select-none">{{ store.state.user?.silver }}</div>
-            <div class="tooltip-text tooltip-bottom">Silver</div>
-        </div>
-        <div class="flex items-center justify-center w-fit tooltip-container">
-            <img src="../assets/items/gold.jpg" class="w-10">
-            <div class="select-none">{{ store.state.user?.gold }}</div>
-            <div class="tooltip-text tooltip-bottom">Gold</div>
-        </div>
-        <div class="flex items-center justify-center w-fit pl-5">
-            <div class="select-none tooltip-container h-full flex items-center">
-                <p>{{ store.state.user?.items.length }}</p>
-                <div class="tooltip-text tooltip-bottom">Items</div>
+    <div class="flex flex-col h-screen w-full">
+        <div class="h-14 flex border-b-black border-b-2">
+            <div class="flex items-center justify-center w-fit tooltip-container">
+                <img src="../assets/items/silver.jpg" class="w-10">
+                <div class="select-none">{{ store.state.user?.silver }}</div>
+                <div class="tooltip-text tooltip-bottom">Silver</div>
             </div>
-            <img src="../assets/economic.png" class="w-10">
-            <div class="select-none tooltip-container h-full flex items-center">
-                <p>{{ store.state.user?.imports.length }}</p>
-                <div class="tooltip-text tooltip-bottom">Imports</div>
+            <div class="flex items-center justify-center w-fit tooltip-container">
+                <img src="../assets/items/gold.jpg" class="w-10">
+                <div class="select-none">{{ store.state.user?.gold }}</div>
+                <div class="tooltip-text tooltip-bottom">Gold</div>
             </div>
+            <div class="flex items-center justify-center w-fit pl-5">
+                <div class="select-none tooltip-container h-full flex items-center">
+                    <p>{{ store.state.user?.items.length }}</p>
+                    <div class="tooltip-text tooltip-bottom">Items</div>
+                </div>
+                <img src="../assets/economic.png" class="w-10">
+                <div class="select-none tooltip-container h-full flex items-center">
+                    <p>{{ store.state.user?.imports.length }}</p>
+                    <div class="tooltip-text tooltip-bottom">Imports</div>
+                </div>
+            </div>
+        </div>
+        <div class="grow flex w-full">
+            <img src="../assets/world.jpg" class="object-cover w-2/3">
+            <div class="bg-white w-1/3 h-full border-l-2 border-l-black"></div>
         </div>
     </div>
 </template>
