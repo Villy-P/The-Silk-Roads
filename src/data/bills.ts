0ea1cba93b91c70c1ccb52d51cd7cde0be147278
Villy-P <valeriuspetrini@gmail.com>
2023-09-18 22:47:53 -0500

    Bruges

---
+++ src/data/bills.ts
@@ -0,0 +1,11 @@
+import { CITIES } from "./city";
+
+export function canBuyBill(city: CITIES) {
+    switch (city) {
+        case CITIES.BRUGES:
+            return true;
+    }
+    return false;
+}
+
+export const bill = `{} is also a major financial center thanks to new banking and credit practices. You may now exchange your bills for silver or gold or get a loan (credit) of up to 20 silver coins (15 gold) if you are in need of money (but don’t stay in debt!)`
--- src/data/city.ts
+++ src/data/city.ts
@@ -44,6 +44,8 @@ export function getCityDescription(city: CITIES) {
             return "Despite being sacked and destroyed by the Mongols in 1258, Baghdad continued to be an important hub on the Silk Roads, connected to both overland and maritime trade routes. Under Mongol ruler Hulegu Khan, the city regained its renown as an intellectual and cultural center. Polymath Nasir al-Din al-Tusi was able to save about 40,000 manuscripts from the House of Wisdom, and intellectual and cultural exchanges continued alongside commercial ones. Because Baghdad is connected to so many trade routes, there are many items from which to choose, and Baghdad seems to have more imported goods for resale than the origin countries from which those goods came.";
         case CITIES.BEIJING:
             return "When Marco Polo visited Beijing, he claimed that<br><b>“In every suburb there are many fine hostels which provide lodging for merchants coming from different parts; a particular hostel is assigned to every nation. . . . Merchants and others come here on business in great numbers, both because it is the Khan's residence and because it affords a profitable market...That is why the volume and value of the imports and of the internal trade exceed those of any other city in the world. It is a fact that every day more than 1,000 cart-loads of silk is woven here. All the treasures that come from India - precious stones, pearls, sugar, cotton, gold, ivory and other rarities- are brought here. So too are the choicest products of Cathay [China] itself and every other province.”</b><br>You consult your Merchant Handbook, written by Italian Francesco Pegolotti. His book is the go to for merchants who need to know about the goods and exchanges in major Silk Road cities. In Beijing <br><b>“Cathay (China) is a province which contained a multitude of cities and towns. Among others there is one in particular, that is to say the capital city, to which is a great resort of merchants, and in which there is a vast amount of trade; and this city is called Cambalec (Beijing). And the said city has a circuit of one hundred miles, and is all full of people and houses and of dwellers in the said city...Whatever silver the merchants may carry with them as far as Cathay the lord of Cathay will take from them and put into his treasury in exchange for paper money...and with this money you can readily buy silk and all other merchandise that you have a desire to buy. And I assure you that all the peoples and populations who are subject to his rule are perfectly willing to accept these papers as payment, since wherever they go they pay in the same currency, whether for goods of pearls or precious stones or gold or silver. With these pieces of paper you can buy anything and pay for anything…”</b>";
+        case CITIES.BRUGES:
+            return "Waves of European travelers have set out across the Silk Roads to see for themselves the great wealth of China and the Mongol khanates, and Europe’s commerce and mercantile economy are growing. Bruges’ trade grew due to its involvement with the Hanseatic League, but in the 13th century it established trade ties with Genoa and then Venice, providing access to the Mediterranean trade and its luxury goods. Bruges became known for its wool manufactures and its stock exchange (perhaps the first in the world). Bruges became the prime hub of international trade in north-western Europe.";
     }
 }
 
@@ -59,6 +61,13 @@ export function getCityImages(city: CITIES) {
                 }, {
                     name: "Arch Bridges Of Taicang, built by the Yuan Dynasty",
                     src: 'arch_bridges_of_taicang.png'
+                }
+            ]};
+        case CITIES.BRUGES:
+            return {items: [
+                {
+                    name: "Brudges Canal",
+                    src: 'bruges_canal.png'
                 }
             ]};
     }
@@ -164,6 +173,37 @@ export function getTradingItems(city: CITIES): TradingItems | undefined {
                     displayName: "Musk (Perfume)"
                 },  
             ]}
+        case CITIES.BRUGES:
+            return {items: [
+                {
+                    item: ITEMS.SPICES,
+                    silver: 15,
+                    gold: 12,
+                    displayName: "Spices from Asia"
+                }, {
+                    item: ITEMS.SILK,
+                    silver: 12,
+                    gold: 10,
+                    displayName: "Silk from China & Italy"
+                }, {
+                    item: ITEMS.SUGAR,
+                    silver: 10,
+                    gold: 8,
+                    displayName: "Sugar from the Mediterranean"
+                }, {
+                    item: ITEMS.SILVER,
+                    silver: 10,
+                    gold: 8,
+                }, {
+                    item: ITEMS.GOLD,
+                    silver: 10,
+                    gold: 8
+                }, {
+                    item: ITEMS.FURS,
+                    silver: 5,
+                    gold: 3
+                },
+            ]};
     }
 }
 
@@ -173,4 +213,5 @@ export function getCityInnovationCard(city: CITIES) {
         case CITIES.BEIJING:
             return true
     }
+    return false;
 }
