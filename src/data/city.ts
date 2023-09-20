fb410ec337e0bf3a7d9b81b52d7a7a3166319824
Villy-P <valeriuspetrini@gmail.com>
2023-09-19 22:51:14 -0500

    calicut

--- src/data/city.ts
+++ src/data/city.ts
@@ -46,13 +46,13 @@ export function getCityDescription(city: CITIES) {
             return "When Marco Polo visited Beijing, he claimed that<br><b>“In every suburb there are many fine hostels which provide lodging for merchants coming from different parts; a particular hostel is assigned to every nation. . . . Merchants and others come here on business in great numbers, both because it is the Khan's residence and because it affords a profitable market...That is why the volume and value of the imports and of the internal trade exceed those of any other city in the world. It is a fact that every day more than 1,000 cart-loads of silk is woven here. All the treasures that come from India - precious stones, pearls, sugar, cotton, gold, ivory and other rarities- are brought here. So too are the choicest products of Cathay [China] itself and every other province.”</b><br>You consult your Merchant Handbook, written by Italian Francesco Pegolotti. His book is the go to for merchants who need to know about the goods and exchanges in major Silk Road cities. In Beijing <br><b>“Cathay (China) is a province which contained a multitude of cities and towns. Among others there is one in particular, that is to say the capital city, to which is a great resort of merchants, and in which there is a vast amount of trade; and this city is called Cambalec (Beijing). And the said city has a circuit of one hundred miles, and is all full of people and houses and of dwellers in the said city...Whatever silver the merchants may carry with them as far as Cathay the lord of Cathay will take from them and put into his treasury in exchange for paper money...and with this money you can readily buy silk and all other merchandise that you have a desire to buy. And I assure you that all the peoples and populations who are subject to his rule are perfectly willing to accept these papers as payment, since wherever they go they pay in the same currency, whether for goods of pearls or precious stones or gold or silver. With these pieces of paper you can buy anything and pay for anything…”</b>";
         case CITIES.BRUGES:
             return "Waves of European travelers have set out across the Silk Roads to see for themselves the great wealth of China and the Mongol khanates, and Europe’s commerce and mercantile economy are growing. Bruges’ trade grew due to its involvement with the Hanseatic League, but in the 13th century it established trade ties with Genoa and then Venice, providing access to the Mediterranean trade and its luxury goods. Bruges became known for its wool manufactures and its stock exchange (perhaps the first in the world). Bruges became the prime hub of international trade in north-western Europe.";
+        case CITIES.CALICUT:
+            return "Calicut (aka Kerala) is a cosmopolitan city of the Malabar Coast of India under control of the Hindu Vijayanagara Empire. It is part of the maritime Silk Road network, and its thriving trade attracts merchants from China, Southeast Asia, Persia, Arabia, East Africa, and other parts of India flock here. Ibn Battuta marveled at the huge Chinese junk ships that brought China’s finest goods here, and Marco Polo noted that this kingdom had “a great quantity of pepper, and ginger, and cinnamon, and of nuts of India. The ships that come from the east [Asia] bring…cloths of silk and gold, and sendels; also gold and silver, cloves, and other fine spices for which there is a demand here, and exchange them for the products of these countries.” Ships can easily access this port from the Red Sea region (Aden, Mecca, East Africa), and goods from here are sold as far away as Venice.";
     }
 }
 
 export function getCityImages(city: CITIES) {
     switch (city) {
-        case CITIES.BAGHDAD:
-            return {items: []};
         case CITIES.BEIJING:
             return {items: [
                 {
@@ -71,6 +71,7 @@ export function getCityImages(city: CITIES) {
                 }
             ]};
     }
+    return {items: []};
 }
 
 interface TradingItem {
@@ -203,6 +204,35 @@ export function getTradingItems(city: CITIES): TradingItems | undefined {
                     silver: 5,
                     gold: 3
                 },
+            ]};
+        case CITIES.CALICUT:
+            return {items: [
+                {
+                    item: ITEMS.SILK,
+                    silver: 7,
+                    gold: 5,
+                }, {
+                    item: ITEMS.PORCELAIN,
+                    silver: 10,
+                    gold: 8,
+                }, {
+                    item: ITEMS.SPICES,
+                    silver: 12,
+                    gold: 10,
+                }, {
+                    item: ITEMS.FINE_CLOTHS,
+                    silver: 3,
+                    gold: 1,
+                }, {
+                    item: ITEMS.HORSES,
+                    silver: 7,
+                    gold: 5,
+                    displayName: "Persian Horses"
+                }, {
+                    item: ITEMS.PRECIOUS_STONES,
+                    silver: 5,
+                    gold: 3,
+                }, 
             ]};
     }
 }
--- src/data/culture.ts
+++ src/data/culture.ts
@@ -4,23 +4,31 @@ export function getCultureCard(city: CITIES) {
     switch (city) {
         case CITIES.BEIJING:
             return "Buddhism and Daoism flourished in China for centuries, but Islam also arrived in Tang China as trade increased with Arabs on the Silk Roads. Under the Mongols,  Muslims served as administrators, generals, and other leaders who were transferred to China from Persia and Central Asia. There are 70 mosques in Beijing alone, and there are about 25 million Muslims in China.<br>In the Middle Ages, Christianity also played an important role in China. Nestorian Christians arrived in Tang China in the 7th century, memorialized in the Xi'an Stele—a Tang monument that documents the presence of Christian communities in China. In the 13th century, numerous Christian missionaries and monks traveled to China, including  John of Montecorvino, sent by the pope to meet with Kublai Khan. He built the first Catholic Church in Beijing in 1305 and boasted thousands of converts. "
+        case CITIES.CALICUT:
+            return "The Vijayanagara kings are also very culturally tolerant, and at this time the Bhakti (devotion) movement is growing popular in India. The Bhakti movement emphasizes a personal relationship with and devotion to a particular Hindu deity (such as Shiva or Vishnu). Devotional practices could include singing hymns of praise to the deity, writing devotional poems, or taking a pilgrimage to a sacred site associated with the deity. While in Calicut, you see the Virupaksha Temple (below), built by a Vijayanagara emperor and devoted to Lord Virupaksha, a local form of Shiva.";
     }
     return null;
 }
 
 export function getCultureCardImages(city: CITIES) {
     switch (city) {
-        case CITIES.BAGHDAD:
-            return {items:[]};
         case CITIES.BEIJING:
             return {items: [
                 {
                     name: "Niujie Mosque in Beijing, the oldest mosque in the city",
-                    src: '@/assets/culture/niujie_mosque.png'
+                    src: 'niujie_mosque.png'
                 }, {
                     name: "The Cathedral of Immaculate Conception in Beijing",
-                    src: '@/assets/culture/immaculate_conception_cathedral.png'
+                    src: 'immaculate_conception_cathedral.png'
+                }
+            ]};
+        case CITIES.CALICUT:
+            return {items: [
+                {
+                    name: "Virupaksha Temple",
+                    src: 'virupaksha_temple.png'
                 }
             ]};
     }
+    return {items:[]};
 }
