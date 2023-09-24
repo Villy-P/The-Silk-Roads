e20beb416ad7d2d596b8a38066662c7c0583e4f0
Villy-P <valeriuspetrini@gmail.com>
2023-09-24 15:58:09 -0500

    CollectInnovation

---
+++ src/components/CollectInnovation.vue
@@ -0,0 +1,79 @@
+<template>
+    <div v-if="getCityInnovationCard() && !store.state.user?.cityInnovations.includes(store.state.user!.currentCity!)">
+        <div class="w-11/12 m-auto text-center py-5">{{ getInnovationCardSpecialText() }}</div>
+        <div class="w-full flex h-fit m-auto items-center">
+            <div class="h-full w-1/12 flex items-center cursor-pointer" @click="changeInnovationCard(-1)">
+                <img src="../assets/icon/leftarrow.svg">
+            </div>
+            <div class="rounded-xl relative w-10/12 mx-3 border-2 border-black h-fit">
+                <div class="citywelcome-warning tooltip-container tooltip-container-abs" v-if="store.state.user?.imports.includes(currentInnovation)">
+                    <p>!</p>
+                    <div class="tooltip-text tooltip-left">You need this item</div>
+                </div>
+                <div class="flex gap-2 items-center w-fit m-auto p-2">
+                    <img :src="require(`@/assets/items/${getItemAsset()}`)" class="w-6 h-6">
+                    <p>{{ getInnovationCardName() }}</p>
+                </div>
+                <div class="p-3 indent-8 overflow-auto h-fit">{{ getInnovationDescription() }}</div>
+                <div class="m-auto px-2 py-1 mb-2 border-2 border-black w-fit bg-blue-400 cursor-pointer" @click="selectInnovation">
+                    Select this Innovation
+                </div>
+            </div>
+            <div class="h-full w-1/12 flex items-center cursor-pointer" @click="changeInnovationCard(1)">
+                <img src="../assets/icon/rightarrow.svg">
+            </div>
+        </div>
+    </div>
+</template>
+
+<!-- eslint-disable @typescript-eslint/no-non-null-assertion -->
+<script lang="ts">
+    import { getCityInnovationCard } from '@/data/city';
+import { getInnovationCardSpecialText } from '@/data/innovation';
+import { ITEMS, getInnovationDescription, getItemAsset, getItemName } from '@/data/items';
+    import { key } from '@/store/store';
+    import { Vue } from 'vue-class-component';
+    import { useStore } from 'vuex';
+
+    export default class CollectInnovation extends Vue {
+        store = useStore(key);
+
+        currentInnovation = ITEMS.TEXTS;
+
+        getInnovationCardName() {
+            return getItemName(this.currentInnovation);
+        }
+
+        getItemAsset() {
+            return getItemAsset(this.currentInnovation);
+        }
+
+        getInnovationDescription() {
+            return getInnovationDescription(this.currentInnovation);
+        }
+
+        getInnovationCardSpecialText() {
+            return getInnovationCardSpecialText(this.store.state.user!.currentCity!);
+        }
+
+        getCityInnovationCard() {
+            return getCityInnovationCard(this.store.state.user!.currentCity!);
+        }
+
+        changeInnovationCard(amount: number) {
+            this.currentInnovation += amount;
+            if (this.currentInnovation < 0)
+                this.currentInnovation = ITEMS.PAPER_MAKING;
+            if (this.currentInnovation > ITEMS.PAPER_MAKING)
+                this.currentInnovation = ITEMS.TEXTS;
+        }
+
+        selectInnovation() {
+            this.store.state.user?.items.push(this.currentInnovation);
+            if (this.store.state.user?.imports.includes(this.currentInnovation))
+                this.store.state.user?.imports.splice(this.store.state.user?.imports.indexOf(this.currentInnovation), 1);
+            this.store.state.user?.cityInnovations.push(this.store.state.user!.currentCity!);
+            this.store.state.socket?.emit('updateUser', this.store.state.user);
+        }
+    }
+</script>
--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -14,31 +14,7 @@
                         <div class="tooltip-text tooltip-top">{{ image.name }}</div>
                     </div>
                 </div>
-                <div v-if="getCityInnovationCard() && !store.state.user?.cityInnovations.includes(store.state.user!.currentCity!)">
-                    <div class="w-11/12 m-auto text-center py-5">{{ getInnovationCardSpecialText() }}</div>
-                    <div class="w-full flex h-fit m-auto items-center">
-                        <div class="h-full w-1/12 flex items-center cursor-pointer" @click="changeInnovationCard(-1)">
-                            <img src="../assets/icon/leftarrow.svg">
-                        </div>
-                        <div class="rounded-xl relative w-10/12 mx-3 border-2 border-black h-fit">
-                            <div class="citywelcome-warning tooltip-container tooltip-container-abs" v-if="store.state.user?.imports.includes(currentInnovation)">
-                                <p>!</p>
-                                <div class="tooltip-text tooltip-left">You need this item</div>
-                            </div>
-                            <div class="flex gap-2 items-center w-fit m-auto p-2">
-                                <img :src="require(`@/assets/items/${getItemAsset()}`)" class="w-6 h-6">
-                                <p>{{ getInnovationCardName() }}</p>
-                            </div>
-                            <div class="p-3 indent-8 overflow-auto h-fit">{{ getInnovationDescription() }}</div>
-                            <div class="m-auto px-2 py-1 mb-2 border-2 border-black w-fit bg-blue-400 cursor-pointer" @click="selectInnovation">
-                                Select this Innovation
-                            </div>
-                        </div>
-                        <div class="h-full w-1/12 flex items-center cursor-pointer" @click="changeInnovationCard(1)">
-                            <img src="../assets/icon/rightarrow.svg">
-                        </div>
-                    </div>
-                </div>
+                <CollectInnovation/>
                 <CollectCulture/>
                 <PayBills/>
                 <div class="m-auto px-2 py-1 mb-2 border-2 border-black w-fit bg-blue-400 cursor-pointer">
@@ -51,9 +27,7 @@
 
 <!-- eslint-disable @typescript-eslint/no-non-null-assertion -->
 <script lang="ts">
-    import { getCityDescription, getCityImages, getCityInnovationCard, getCityName } from '@/data/city';
-    import { getInnovationCardSpecialText } from '@/data/innovation';
-    import { ITEMS, getItemAsset, getItemName, getInnovationDescription } from '@/data/items';
+    import { getCityDescription, getCityImages, getCityName } from '@/data/city';
     import { key } from '@/store/store';
     import { Vue, Options } from 'vue-class-component';
     import { useStore } from 'vuex';
@@ -61,6 +35,7 @@
     import WorldMap from './WorldMap.vue';
     import CollectCulture from './CollectCulture.vue';
     import PayBills from './PayBills.vue';
+    import CollectInnovation from './CollectInnovation.vue';
 
     @Options({
         components: {
@@ -68,12 +43,11 @@
             WorldMap,
             CollectCulture,
             PayBills,
+            CollectInnovation
         }
     })
     export default class MainView extends Vue {
         store = useStore(key);
-
-        currentInnovation = ITEMS.TEXTS;
 
         getCityName() {
             return getCityName(this.store.state.user!.currentCity!);
@@ -83,44 +57,8 @@
             return getCityDescription(this.store.state.user!.currentCity!);
         }
 
-        getInnovationCardSpecialText() {
-            return getInnovationCardSpecialText(this.store.state.user!.currentCity!);
-        }
-
-        getCityInnovationCard() {
-            return getCityInnovationCard(this.store.state.user!.currentCity!);
-        }
-
-        getInnovationCardName() {
-            return getItemName(this.currentInnovation);
-        }
-
-        getItemAsset() {
-            return getItemAsset(this.currentInnovation);
-        }
-
-        getInnovationDescription() {
-            return getInnovationDescription(this.currentInnovation);
-        }
-
-        changeInnovationCard(amount: number) {
-            this.currentInnovation += amount;
-            if (this.currentInnovation < 0)
-                this.currentInnovation = ITEMS.PAPER_MAKING;
-            if (this.currentInnovation > ITEMS.PAPER_MAKING)
-                this.currentInnovation = ITEMS.TEXTS;
-        }
-
         getCityImages() {
             return getCityImages(this.store.state.user!.currentCity!);
-        }
-
-        selectInnovation() {
-            this.store.state.user?.items.push(this.currentInnovation);
-            if (this.store.state.user?.imports.includes(this.currentInnovation))
-                this.store.state.user?.imports.splice(this.store.state.user?.imports.indexOf(this.currentInnovation), 1);
-            this.store.state.user?.cityInnovations.push(this.store.state.user!.currentCity!);
-            this.store.state.socket?.emit('updateUser', this.store.state.user);
         }
     }
 </script>
