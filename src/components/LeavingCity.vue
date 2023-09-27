229200c5c12b07371997dcaa23c413819d791e7b
Villy-P <valeriuspetrini@gmail.com>
2023-09-26 23:27:45 -0500

    Leave City

--- src/components/LeavingCity.vue
+++ src/components/LeavingCity.vue
@@ -5,16 +5,23 @@
     <div class="w-10/12 bg-cyan-400 m-auto border-2 border-black py-2 px-4 cursor-pointer hover:bg-cyan-500 mb-5" style="font-variant: small-caps;" @click="goBack">
         <p>Go Back Inside</p>
     </div>
+    <div v-for="city in getCityConnections()" 
+         :key="city" 
+         class="w-10/12 bg-cyan-400 m-auto border-2 border-black py-2 px-4 cursor-pointer hover:bg-cyan-500 mb-5" 
+         style="font-variant: small-caps;" 
+         @click="goToCity(city)">
+        <p>Travel to {{ getCityNameWithI(city) }}</p>
+    </div>
 </template>
 
 <!-- eslint-disable @typescript-eslint/no-non-null-assertion -->
 <script lang="ts">
-    import { getCityName } from '@/data/city';
+    import { CITIES, getCityName } from '@/data/city';
     import { GAME_STATE } from '@/scripts/state';
     import { key } from '@/store/store';
     import { Vue } from 'vue-class-component';
     import { useStore } from 'vuex';
-    import {  } from '@/data/city';
+    import { getCityConnections } from '@/data/city';
 
     export default class LeavingCity extends Vue {
         store = useStore(key);
@@ -23,14 +30,30 @@
             return getCityName(this.store.state.user!.currentCity!);
         }
 
+        getCityNameWithI(i: CITIES) {
+            return getCityName(i);
+        }
+
         goBack() {
             this.store.state.user!.state = GAME_STATE.IN_CITY;
+            this.store.state.socket?.emit('updateUser', this.store.state.user);
+        }
+
+        goToCity(city: CITIES) {
+            this.store.state.user!.state = GAME_STATE.WELCOME_TO_CITY;
+            this.store.state.user!.hasMarketTransactions = false;
+            this.store.state.user!.hasMarketTransactions = false;
+            this.store.state.user!.currentCity = city;
             this.store.state.socket?.emit('updateUser', this.store.state.user);
         }
 
         leaveCity() {
             this.store.state.user!.state = GAME_STATE.LEAVING;
             this.store.state.socket?.emit('updateUser', this.store.state.user);
+        }
+
+        getCityConnections() {
+            return getCityConnections(this.store.state.user!.currentCity!);
         }
     }
 </script>
