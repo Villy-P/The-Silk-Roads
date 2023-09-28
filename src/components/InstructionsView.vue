2fbba01ced1b11316f6e4e89316ec34f44a01a77
Villy-P <valeriuspetrini@gmail.com>
2023-09-28 12:30:15 -0500

    Debt Instructions

--- src/components/InstructionsView.vue
+++ src/components/InstructionsView.vue
@@ -14,6 +14,8 @@
     <p class="p">In each city, there is a market. In those markets is where you can purchase or sell certain goods. You can buy and sell goods at the same price, however some cities may have better deals than others. During each city visit, you can only buy/sell one product (If you leave and return, then you can still buy/sell again). If you currently need an item, a warning sign will appear next to that item showing that you can collect it.</p>
     <h1 class="h1">World Map</h1>
     <p class="p">The World Map on the left shows all cities that you can move to. A yellow map tack shows you where you currently are. You can move the map by dragging/scrolling</p>
+    <h1 class="h1">Bills and Debt</h1>
+    <p class="p">If you are in need of silver/gold, and you have a bank note in your inventory, you may trade them at certain cities for a loan of 20 silver or 10 gold. You instantly get this money, however you are now "in debt" and must pay this back to the lender. In order to do this you may click on the debt tab in the top right corner. The lender may demand the debt back at any time. If you are in need of a bank note, go to the debt tab and take one out.</p>
 </template>
 
 <!-- eslint-disable @typescript-eslint/no-non-null-assertion -->
